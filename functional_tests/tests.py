26d0d59db3b59c81fac602971bb3b48426abcaf4
Ken Ingram <ken.ingram@gmail.com>
2021-09-24 19:16:00 -0700

    Update functional test.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,14 +19,14 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDown(self):
     self.browser.quit()
 
-  def wait_row_in_list_table(self, row_text):
+  def wait_for_row_in_list_table(self, row_text): # from page 90
     start_time = time.time()
     while True:
       try:
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-      except:
+      except (AssertionError, WebDriverException) as e:
         if time.time() - start_time > MAX_WAIT:
           raise e
         time.sleep(0.5)
@@ -56,23 +56,22 @@ class NewVisitorTest(LiveServerTestCase):
     # When she hits enter, the page updates, annd now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list table
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
-    self.wait_row_in_list_table('1: Buy peacock feathers')
+    self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+    #time.sleep(1)
     
     # The page updates again, annd now shows both items on her list
     # table = self.browser.find_element_by_id('id_list_table')
     # rows = table.find_elements_by_tag_name('tr')
     # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
     # self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-    self.wait_row_in_list_table('1: Buy peacock feathers')
-    self.wait_row_in_list_table('2: Use peacock feathers to make a fly')
+    self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
     # Edith wonders whether the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some 
